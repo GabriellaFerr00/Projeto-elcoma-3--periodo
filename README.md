f3b68a2023de5a60c04c29315efe9f4d5a1a3cd8
Gabriella <gabriellalok3@gmail.com>
2022-06-09 10:59:26 -0300

    projeto elcoma final

---
+++ README.md
@@ -0,0 +1,2 @@
+# elcomaProjeto
+Projeto FullStack
---
+++ src/main/java/com/cadastro/models/Usuario.java
@@ -0,0 +1,150 @@
+package com.cadastro.models;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_USUARIO")
+public class Usuario implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	//auto incremento da chave id
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	@Column(name = "ID")
+	private long codigo;
+	
+	@Column(name = "NOME")
+	private String nome;
+	
+	@Column(name = "SOBRENOME")
+	private String sobrenome;
+	
+	@Column(name = "UF")
+	private String uf;
+	
+	@Column(name = "CIDADE")
+	private String cidade;
+	
+	@Column(name = "DATA_NASCIMENTO")
+	private String dataNascimento;
+	
+	@Column(name = "CPF")
+	private String cpf;
+	
+	@Column(name = "GENERO")
+	private String genero;
+	
+	@Column(name = "EMAIL")
+	private String email;
+	
+	@Column(name = "DDD")
+	private int ddd;
+	
+	@Column(name = "TELEFONE")
+	private String telefone;
+
+	@Column(name = "SENHA")
+	private String senha;
+
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getDdd() {
+		return ddd;
+	}
+
+	public void setDdd(int ddd) {
+		this.ddd = ddd;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
